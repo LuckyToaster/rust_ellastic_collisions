298bc81b9da2c05b97eb82da5a36d5e76df8a283
LuckyToast <LuckyToast@github.com>
2023-09-09 17:28:02 +0300

    fixed test

--- src/tests.rs
+++ src/tests.rs
@@ -8,9 +8,9 @@ fn util_collide_distance_particle_update() {
     let vel = v(15.0, 15.0);
     let acc = v(-15.0, -15.0);
 
-    let mut p1 = Particle::new(pos, vel, acc, 6.0, 10, Color::BLACK);
-    let mut p2 = Particle::new(pos, acc, acc, 6.0, 10, Color::BLACK);
-    let p3 = Particle::new(pos, vel, acc, 6.0, 10, Color::BLACK);
+    let mut p1 = Particle::new(pos, vel, acc, 6.0, 10.0, Color::BLACK);
+    let mut p2 = Particle::new(pos, acc, acc, 6.0, 10.0, Color::BLACK);
+    let p3 = Particle::new(pos, vel, acc, 6.0, 10.0, Color::BLACK);
 
     assert_eq!(&p1, &p3);
     assert_ne!(&p1, &p2);
@@ -29,8 +29,8 @@ fn util_collide_distance_particle_update() {
 
 #[test] 
 fn util_overlap_solve_overlap() {
-    let mut p1 = Particle::new(v(5.0, 10.0), v(0.0, 0.0), v(0.0, 0.0), 100.0, 10, rcolor());  
-    let mut p2 = Particle::new(v(10.0, 10.0), v(0.0, 0.0), v(0.0, 0.0), 100.0, 10, rcolor());  
+    let mut p1 = Particle::new(v(5.0, 10.0), v(0.0, 0.0), v(0.0, 0.0), 100.0, 10.0, rcolor());  
+    let mut p2 = Particle::new(v(10.0, 10.0), v(0.0, 0.0), v(0.0, 0.0), 100.0, 10.0, rcolor());  
 
     assert_eq!(distance(&p1, &p2), 5.0);
     assert_eq!(overlap(&p1, &p2), 195.0);
@@ -39,7 +39,6 @@ fn util_overlap_solve_overlap() {
 
     assert_eq!(p1.p.x, 5.0 - 195.0 * 0.5);
     assert_eq!(p2.p.x, 10.0 + 195.0 * 0.5);
-
 }
 
 
